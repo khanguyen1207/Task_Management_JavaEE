83c0c6a736caf29b77ff6fa58330ed538f7e36ce
Kha Nguyen <kha.nguyen@smarp.com>
2017-02-23 23:43:26 +0200

    adding springsocial

---
+++ src/main/java/com/main/SocialConfig.java
@@ -0,0 +1,18 @@
+package com.main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+
+/**
+ * Created by khanguyen on 23/02/2017.
+ */
+@Configuration
+public class SocialConfig {
+//    @Bean
+//    public ConnectionFactoryLocator connectionFactoryLocator() {
+//        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+//        registry.addConnectionFactory( new FacebookConnecti);
+//    }
+}
