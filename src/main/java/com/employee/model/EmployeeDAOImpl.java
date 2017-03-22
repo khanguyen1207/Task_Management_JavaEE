5f5b2a6d3a66b432bb24a3c8d551424cd41303a0
Kha Nguyen <khanguyen@gmail.com>
2017-03-23 01:19:06 +0200

    add delete and save methods

--- src/main/java/com/employee/model/EmployeeDAOImpl.java
+++ src/main/java/com/employee/model/EmployeeDAOImpl.java
@@ -1,7 +1,13 @@
 package com.employee.model;
 
+import com.google.gson.Gson;
+import org.json.JSONObject;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class EmployeeDAOImpl {
     final private String appUrl = "https://ascendant-timer-158814.firebaseio.com/employee/";
@@ -18,9 +24,46 @@ public class EmployeeDAOImpl {
         }
     }
 
-    public Long deleteByUsername(String username) {
+    public void deleteByUsername(String username) {
         //TODO: Implement delete function
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        final  String url = appUrl + username + ".json";
+        try {
+            restTemplate.delete(url);
+        } catch (RestClientException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void save(Employee employee) {
+        RestTemplate restTemplate = new RestTemplate();
+        final String url = appUrl + employee.getUsername() + ".json";
+        try {
+            restTemplate.put(url, employee);
+        } catch (RestClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Employee> findAll() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<Employee> employees = new ArrayList<>();
+        try {
+            String jsonString = restTemplate.getForObject(appUrl + ".json", String.class);
+            JSONObject jsonObject = new JSONObject(jsonString.trim());
+            Iterator<?> keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                if (jsonObject.get(key) instanceof JSONObject) {
+                    System.out.println(jsonObject.get(key).toString());
+                    employees.add(new Gson().fromJson(jsonObject.get(key).toString(), Employee.class));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
+
 
 }
